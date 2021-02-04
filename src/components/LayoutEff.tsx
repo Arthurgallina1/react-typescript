ad1c84a2e04e2a2c6d6bc89aabbae3c522b85dcf
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-02-04 12:18:33 -0300

    /

--- src/components/LayoutEff.tsx
+++ src/components/LayoutEff.tsx
@@ -1,22 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useLayoutEffect, useEffect } from "react";
+
+export interface Todo {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+}
 
 export default function LayoutEff() {
     const [description, setDescription] = useState("");
+    const [todo, setTodo] = useState<Todo>();
+
+    useLayoutEffect(() => {}, []);
     useEffect(() => {
-        let currentRender = true;
-        setTimeout(() => {
-            if (currentRender) {
-                console.log(description);
-            } else {
-                console.log("ops im done");
-            }
-        }, 1500);
-        return () => {
-            currentRender = false;
-        };
+        console.log("ly2");
+        async function loadData() {
+            const resp = await fetch(
+                "https://jsonplaceholder.typicode.com/todos/" + description
+            );
+            const json = await resp.json();
+            setTodo(json);
+            console.log(json);
+        }
+        loadData();
+        //ex 1
+        // let currentRender = true;
+        // setTimeout(() => {
+        //     if (currentRender) {
+        //         console.log(description);
+        //     } else {
+        //         console.log("ops im done");
+        //     }
+        // }, 1500);
+        // return () => {
+        //     currentRender = false;
+        // };
 
         // both do the same thing. ^ in case we don't have a callback from the call
-
+        // ex 2
         // const id = setTimeout(() => {
         //     console.log(description);
         // }, 1500);
@@ -27,6 +48,7 @@ export default function LayoutEff() {
     }, [description]);
     return (
         <div>
+            {todo?.title}
             <input
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
