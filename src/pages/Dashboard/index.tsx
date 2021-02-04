f7a23e4acb29953fec552805d3e96ce50f20e931
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-02-03 22:58:44 -0300

    add interfaces

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,14 +4,62 @@ import useConsole from "../../hooks/useConsole";
 import * as Styles from "./styles";
 //import * as S or { Container } ...
 import api from "../../services/api";
+enum Status {
+    todo,
+    progress,
+    complete,
+}
+
+let todoStatus: Status = Status.todo;
+
+let notSure: unknown = 4;
 
 interface ITodo {
     id: number;
-    completed: boolean;
+    status: Status;
     title: string;
     userId: number;
 }
+
 type todoArray = ITodo[];
+
+interface LabeledValue {
+    label: string;
+    color?: string;
+}
+
+function printLabel(labeledObj: LabeledValue) {
+    console.log(labeledObj.label);
+}
+let myObj = { size: 10, label: "size 10 obj" };
+printLabel(myObj);
+
+interface SquareConfig {
+    color?: string;
+    width: number;
+}
+
+//interface com funções
+interface SearchFunc {
+    (source: string, subString: string): boolean;
+}
+
+let mySearch: SearchFunc;
+
+mySearch = function (src: string, sub: string): boolean {
+    let result = src.search(sub) < -1;
+    return result;
+};
+
+function createSquare(config: SquareConfig): { color: string; area: number } {
+    let newSq = { color: "white", area: 100 };
+    if (config.color) {
+        newSq.color = config.color;
+    }
+    console.log(newSq);
+    return newSq;
+}
+createSquare({ color: "blue", width: 20 });
 
 function Dashboard() {
     const [todos, setTodos] = useState<todoArray>([]);
@@ -21,11 +69,12 @@ function Dashboard() {
     useEffect(() => {
         const getTodos = async () => {
             const { data } = await api.get("/todos");
-            console.log(data);
             setTodos(data);
         };
         getTodos();
     }, []);
+
+    const getTodo = (todo: ITodo) => {};
 
     const getTodoById = (id: number) => {
         const todo = todos.find((todo) => todo.id === id);
@@ -34,11 +83,11 @@ function Dashboard() {
 
     const updateTodo = (todo: ITodo) => {
         const todon = getTodoById(todo.id);
-        todon.completed = true;
         return todon;
     };
 
-    console.log(getTodoById(3));
+    // let todo: ITodo = getTodoById(3);
+    // console.log(updateTodo(todo));
     return (
         <Styles.Container>
             <Styles.Navbar>Olar</Styles.Navbar>
